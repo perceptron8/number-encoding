1ec9c6401f7eeeda3ab87f9547c90cc48064aa41
perceptron8 <perceptron8@gmail.com>
2015-07-27 17:33:34 +0200

    update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var NumberEncoder = require("./lib/NumberEncoder.js");
 var NumberDecoder = require("./lib/NumberDecoder.js");
 
 module.exports = {
-	NumberEncoder : encoding.NumberEncoder,
-	NumberDecoder : encoding.NumberDecoder,
+	NumberEncoder : NumberEncoder,
+	NumberDecoder : NumberDecoder,
 };
